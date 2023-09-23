b7d326f8f8d0700a41ed7d3ff54aef9c92512169
David White <dawhite4@ncsu.edu>
2023-09-23 14:07:26 -0400

    Made full workflow for metadata uploading to db

--- scripts/metadata_extractor.py
+++ scripts/metadata_extractor.py
@@ -1,5 +1,4 @@
 import os, json
-from db import init, get_all
 
 import pandas as pd
 
@@ -11,7 +10,7 @@ if not os.path.exists(FILE_OUTPUT_PATH):
     os.makedirs(FILE_OUTPUT_PATH)
 
 # Extract metadata dict from a dataframe
-def get_metadata_from_df(df):
+def extract_metadata(df: pd.DataFrame) -> dict:
     metadata = {}
     
     # Compute Values
@@ -24,43 +23,61 @@ def get_metadata_from_df(df):
     col_count = df.shape[1]
     metadata['col_count'] = col_count
 
+    metadata['topic'] = ''
+    metadata['description'] = ''
+    metadata['tags'] = []
+    metadata['licenses'] = []
+
     return metadata
 
+    # id: Optional[int] = Field(default=None, primary_key=True)
+    # topic: str
+    # title: str 
+    # description: str
+    # tags: List[str] = Field(sa_column=JSON)
+    # licenses: List[str] = Field(sa_column=JSON)
+    # percent_null: float 
+    # row_count: int
+    # col_count: int
+
+# Print from metadata to JSON file
 def print_metadata_to_file(metadata, filename):
     full_path = os.path.join(FILE_OUTPUT_PATH, filename)
-    print(full_path)
+    print("Processing", full_path)
 
-    meta_json = json.dumps(metadata)
+    meta_json = json.dumps(metadata, indent=4, sort_keys=True)
 
     with open(full_path, "w+") as newfile:
         newfile.write(meta_json)
-    
 
 # Extract metadata and output
 problem_files = []
 
+# Extract metadat from each csv file to JSON
 for filename in os.listdir(FILE_INPUT_PATH):
     try:
-        # EXTRACT METADATA
+        # EXTRACT METADATA TO DICTIONARY
         file = os.path.join(FILE_INPUT_PATH, filename)
 
         if file[len(file) - 4:] != '.csv':
             continue
 
+        # Check if metadata has already been generated
+        if os.path.exists(os.path.join(FILE_OUTPUT_PATH, str(filename[:len(filename) - 4] + '_metadata.json'))):
+            continue
+
         df = pd.read_csv(file, encoding='unicode_escape')
-        metadata = get_metadata_from_df(df)
+        metadata = extract_metadata(df)
 
         metadata['title'] = filename[:len(filename) - 4]
 
-        # OUTPUT METADATA TO FILE
+        # OUTPUT DICTIONARY TO FILE
         print_metadata_to_file(metadata, str(metadata['title'] + '_metadata.json'))
+
     except Exception:
+        print("E")
         problem_files.append(filename)
 
 with open("problem-files.txt", "w+") as newfile:
     newfile.write(json.dumps(problem_files))
 print("Found ", len(problem_files), " problem files. Look at problem-files.txt for a list.")
-
-# init()
-
-# get_all()
