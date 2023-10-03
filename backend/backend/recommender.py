f959e46090e6bfe053d9b00895acaa1bf0bbb722
Joshua Mason <jlmason6@ncsu.edu>
2023-10-03 10:48:45 -0400

    tentative python 3.9 backport for backend

--- backend/backend/recommender.py
+++ backend/backend/recommender.py
@@ -1,4 +1,26 @@
+import pickle
+from pathlib import Path
+
 from backend.models import Dataset
 
-def rank(datasets: list[Dataset]) -> list[Dataset]:
-    return datasets
+# TODO: add documentation
+class RecomendationModel:
+    _instance = None
+    
+    def __new__(cls, model_dir):
+        if cls._instance == None:
+            cls._instance = super(RecomendationModel, cls).__new__(cls)
+            cls._instance._load_model(model_dir)
+        return cls._instance
+    
+    def _load_model(self, model_dir):
+        with open(model_dir, 'rb') as file:
+            self.model = pickle.load(file)
+        # TODO: catch exceptions if model file doesn't exist
+
+    def rank(self, dataset_of_interest: Dataset) -> list[Dataset]:
+        full_rec_list = self.model.recommendations
+        dataset_recs = full_rec_list[dataset_of_interest.ID]
+        return dataset_recs
+
+recommendation_model = RecomendationModel(Path("models/model.pkl")) # TODO: make this path come frome config or env variable
