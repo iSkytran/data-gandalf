171cb04e2d450fe95f37f2bfb5e31763ab91e76f
Joshua Mason <jlmason6@ncsu.edu>
2023-10-06 10:53:26 -0500

    skeleton unit test for model system

--- model_system/tests/TFIDF_test.py
+++ model_system/tests/TFIDF_test.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 # I don't think the model subsytem should have Unit tests. I still don't get what would be tested.
 # The backend tests should already test out the rank functionality.
+
+def test_something():
+    pass
