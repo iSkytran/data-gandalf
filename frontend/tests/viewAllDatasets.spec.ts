ccd05e567827dd7ce545b880e32d1864a65861d8
ajtermin <ajtermin@ncsu.edu>
2023-10-16 11:34:22 -0400

    Added some frontend test functionality

--- frontend/tests/viewAllDatasets.spec.ts
+++ frontend/tests/viewAllDatasets.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, defineConfig, type Page } from '@playwright/test';
 
+/**
 export default defineConfig({
   // Run your local dev server before starting the tests
   webServer: {
@@ -11,29 +12,24 @@ export default defineConfig({
     baseURL: 'http://localhost:3000'
   }
 });
+*/
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-const TOPICS = [
-  'sports',
-  'academics',
-  'realty',
-  'finance',
-  'health'
-];
+const TOPICS = ['examplecom', 'exmaplenet', 'exampleorg'];
 
 test.describe('View All Datasets', () => {
-  test('Homepage should have Data Gandalf header', async({page}) => {
+  test('Homepage should have title: Data Gandalf', async({page}) => {
     await expect(page).toHaveTitle('Data Gandalf');
   });
 
   test('Homepage should have filter bar with specified topics', async({page}) => {
-    const filterBar = page.getByPlaceholder('Filter By Topic');
+    const filterBar = page.locator('css=topicSelect');
 
     for (let i = 0; i < TOPICS.length; i++) {
-      await expect(filterBar).toHaveAttribute('options', TOPICS[i]);
+      await expect(filterBar.getByText(TOPICS[i])).toBe(TOPICS[i]);
     }
   });
 
