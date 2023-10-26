5a710c477cfc2d760012522bc6b1ab671ec25033
Ethan Nhu Tran <entran@ncsu.edu>
2023-10-26 01:15:37 -0400

    Add fetch calls for posting/deleting user ratings

--- frontend/app/components/rating.tsx
+++ frontend/app/components/rating.tsx
@@ -1,35 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp as solidThumbsUp, faThumbsDown as solidThumbsDown } from "@fortawesome/free-solid-svg-icons";
 import { faThumbsUp as regThumbsUp, faThumbsDown as regThumbsDown } from "@fortawesome/free-regular-svg-icons";
-import { useCookies } from "react-cookie";
-import { v4 as uuidv4 } from "uuid";
 
-export default function Rating({ metadata }: { metadata: any }) {
+export default function Rating({ ratingData, userSession }: { ratingData: any, userSession: string }) {
   // 0 represents no selection, 1 is positive, -1 is negative.
   const [rating, setRating] = useState(0);
-  const [cookies, setCookie] = useCookies(["session-uuid"]);
-  // if (!cookies.some((e: any) => e.name === "session-uuid"])) {
-  //   setCookie("session-uuid", uuidv4());
-  // }
-  // const url = `/api/datasets/${encodeURIComponent(params.dataset)}`;
-  
+  const postUrl = `/api/ratings`;
+  let deleteUrl: string;
+  if (ratingData !== undefined) {
+    deleteUrl = `/api/ratings/${encodeURIComponent(ratingData.id)}`;
+  }
+
+  useEffect(() => {
+    if (ratingData !== undefined) {
+      if (ratingData.recommend === true) {
+        setRating(1);
+      } else if (ratingData.recommend === false) {
+        setRating(-1);
+      }
+    }
+    }, [ratingData]
+  );
 
   const thumbsUp = () => {
     if (rating === 1) {
       setRating(0);
-      // fetch(url, { method: "DELETE" });
+      fetch(deleteUrl, { method: "DELETE" });
     } else {
       setRating(1);
+      fetch(postUrl, {
+        method: "POST",
+        body: JSON.stringify({
+            recommend: true,
+            user_session: userSession,
+            source_dataset: ratingData.source_dataset,
+            destination_dataset: ratingData.destination_dataset,
+        }),
+      });
     }
   };
 
   const thumbsDown = () => {
     if (rating === -1) {
       setRating(0);
-      // fetch(url, { method: "DELETE" });
+      fetch(deleteUrl, { method: "DELETE" });
     } else {
       setRating(-1);
+      fetch(postUrl, {
+        method: "POST",
+        body: JSON.stringify({
+            recommend: true,
+            user_session: userSession,
+            source_dataset: ratingData.source_dataset,
+            destination_dataset: ratingData.destination_dataset,
+        }),
+      });
     }
   };
 
--- frontend/app/dataset/[dataset]/page.tsx
+++ frontend/app/dataset/[dataset]/page.tsx
@@ -3,29 +3,44 @@ import { useState, useEffect } from "react";
 import Grid from "../../components/grid";
 import GridItem from "../../components/gridItem";
 import Rating from "../../components/rating";
+import { useCookies } from "react-cookie";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Dataset({ params }: { params: { dataset: string } }) {
   const [datasets, setDatasets] = useState([]);
+  const [ratings, setRatings] = useState([]);
+  const [cookies, setCookie] = useCookies(["user_session"]);
+  if (!("user_session" in cookies)) {
+    setCookie("user_session", uuidv4());
+  }
 
   useEffect(() => {
     const datasetUrl = `/api/datasets/${encodeURIComponent(params.dataset)}`;
+    const ratingUrl = `/api/ratings/?user_session=${encodeURIComponent(cookies["user_session"])}&source_dataset=${encodeURIComponent(params.dataset)}`;
     fetch(datasetUrl)
       .then((res) => res.json())
       .then((data) => {
         setDatasets(data);
       });
+    fetch(ratingUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setRatings(data);
+      });
   }, [params.dataset]);
-
-  const items = datasets.map((dataset: any) => {
-    return (
-      <GridItem key={dataset.id} metadata={dataset}>
-      </GridItem>
-    );
-  });
 
   if (!datasets) {
     return <div>Loading...</div>;
   }
+
+  const items = datasets.map((dataset: any) => {
+    let ratingData = ratings.find((e: any) => e.destination_dataset === dataset.id);
+    return (
+      <GridItem key={dataset.id} metadata={dataset}>
+        <Rating ratingData={ratingData} userSession={cookies["user_session"]}/>
+      </GridItem>
+    );
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
