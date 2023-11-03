d1523d847e575c44bc46f17ddad05eeead649313
ajtermin <ajtermin@ncsu.edu>
2023-11-02 21:55:56 -0400

    Wrote system tests for UC 3 and 4

--- frontend/tests/frontendTests.spec.ts
+++ frontend/tests/frontendTests.spec.ts
@@ -13,12 +13,12 @@ fetch("http://localhost:3000/api/topics")
     });
   });
 
-let datasets: Array<string> = [];
+let datasets: Array<string[]> = [];
 fetch("http://localhost:3000/api/datasets")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((dataset: any) => {
-      datasets.push(dataset.id + ' ' + dataset.topic + ' ' + dataset.title);      
+      datasets.push([dataset.id, dataset.topic, dataset.title, dataset.description, dataset.licenses, dataset.tags]);    
     });
   });
 
@@ -41,11 +41,10 @@ test.describe('View Available Datasets', () => {
 
   test('Homepage should have a grid of available datasets', async({page}) => {
     const grid = page.locator('_react=Grid');
-    
-    for (let i = 0; i < datasets.length; i++) {
-      const id = datasets[i].substring(0, datasets[i].indexOf(' '));
-      const temp = datasets[i].substring(datasets[i].indexOf(' ') + 1);
-      const title = temp.substring(temp.indexOf(' ') + 1);
+
+    for (let dataset of datasets) {
+      const id = dataset[0];
+      const title = dataset[2];
 
       const gridItem = await grid.locator('_react=GridItem[key = \'' + id + '\']').innerHTML();
 
@@ -65,11 +64,10 @@ test.describe('Filter Available Datasets by Topic', () => {
     await sportsTopic.click();
 
     const grid = page.locator('_react=Grid');
-    
-    for (let i = 0; i < datasets.length; i++) {
-      const id = datasets[i].substring(0, datasets[i].indexOf(' '));
-      const temp = datasets[i].substring(datasets[i].indexOf(' ') + 1);
-      const topic = temp.substring(0, temp.indexOf(' '));
+
+    for (let dataset of datasets) {
+      const id = dataset[0];
+      const topic = dataset[1];
 
       if (topic === 'sports') {
         const gridItem = await grid.locator('_react=GridItem[key = \'' + id + '\']').innerHTML();
@@ -81,13 +79,131 @@ test.describe('Filter Available Datasets by Topic', () => {
 });
 
 test.describe('View Dataset from Homepage', () => {
-  test('Page should have a grid of sports datasets', async({page}) => {
+  test('Dataset page should have appropriate dataset details displayed', async({page}) => {
+    const grid = page.locator('_react=Grid');
+
+    const dataset = datasets[0];
+    const topic = dataset[1];
+    const title = dataset[2];
+    const description = dataset[3];
+    const licenses:any = dataset[4];
+    const tags:any = dataset[5];
+
+    const gridItem = await grid.locator('_react=GridItem').first();
+
+    const titleSelect = await gridItem.getByText(title).first();
+
+    await titleSelect.click();
+    await page.waitForTimeout(2000);
+
+    const gridItemLarge = await page.locator('_react=GridItemLarge').innerHTML();
+
+    await expect(gridItemLarge).toContain(title);
+    await expect(gridItemLarge).toContain(topic);
+    await expect(gridItemLarge).toContain(description);
+    for (let license in licenses) {
+      await expect(gridItemLarge).toContain(license);
+    }
+    for (let tag in tags) {
+      await expect(gridItemLarge).toContain(tag);
+    }
+  });
+
+  test('Dataset page should have recommendations displayed', async({page}) => {
+    const grid = page.locator('_react=Grid');
+
+    const dataset = datasets[0];
+    const title = dataset[2];
+
+    const gridItem = await grid.locator('_react=GridItem').first();
+
+    const titleSelect = await gridItem.getByText(title).first();
+
+    await titleSelect.click();
+    await page.waitForTimeout(2000);
+
+    const recommendationsGrid = page.locator('_react=Grid');
+    const recommendations = await recommendationsGrid.locator('_react=GridItem').all();
+
+    for (let i = 0; i < recommendations.length; i++) {
+      const recommendationHTML = await recommendations[i].innerHTML();
+
+      await expect(recommendationHTML).toContain('Similarity:');
+    }
     
   });
 });
 
 test.describe('View Dataset from Filtered Results', () => {
-  
+  test('Dataset page should have appropriate dataset details displayed', async({page}) => {
+    const filterBar = page.locator('_react=FilterBar');
+    await filterBar.click();
+    
+    const sportsLocators = await page.getByText('sports').all();
+    const sportsTopic = sportsLocators[0];
+    
+    await sportsTopic.click();
+
+    const grid = page.locator('_react=Grid');
+
+    const dataset = datasets[0];
+    const topic = dataset[1];
+    const title = dataset[2];
+    const description = dataset[3];
+    const licenses:any = dataset[4];
+    const tags:any = dataset[5];
+
+    const gridItem = await grid.locator('_react=GridItem').first();
+
+    const titleSelect = await gridItem.getByText(title).first();
+
+    await titleSelect.click();
+    await page.waitForTimeout(2000);
+
+    const gridItemLarge = await page.locator('_react=GridItemLarge').innerHTML();
+
+    await expect(gridItemLarge).toContain(title);
+    await expect(gridItemLarge).toContain(topic);
+    await expect(gridItemLarge).toContain(description);
+    for (let license in licenses) {
+      await expect(gridItemLarge).toContain(license);
+    }
+    for (let tag in tags) {
+      await expect(gridItemLarge).toContain(tag);
+    }
+  });
+
+  test('Dataset page should have recommendations displayed', async({page}) => {
+    const filterBar = page.locator('_react=FilterBar');
+    await filterBar.click();
+    
+    const sportsLocators = await page.getByText('sports').all();
+    const sportsTopic = sportsLocators[0];
+    
+    await sportsTopic.click();
+
+    const grid = page.locator('_react=Grid');
+
+    const dataset = datasets[0];
+    const title = dataset[2];
+
+    const gridItem = await grid.locator('_react=GridItem').first();
+
+    const titleSelect = await gridItem.getByText(title).first();
+
+    await titleSelect.click();
+    await page.waitForTimeout(2000);
+
+    const recommendationsGrid = page.locator('_react=Grid');
+    const recommendations = await recommendationsGrid.locator('_react=GridItem').all();
+
+    for (let i = 0; i < recommendations.length; i++) {
+      const recommendationHTML = await recommendations[i].innerHTML();
+
+      await expect(recommendationHTML).toContain('Similarity:');
+    }
+    
+  });
 });
 
 test.describe('Rate Recommendation Exists', () => {
