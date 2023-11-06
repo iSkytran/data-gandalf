c6c7ee8c3524b93ec84e979575dc584ff71d7c9e
Joshua Mason <jlmason6@ncsu.edu>
2023-11-06 09:53:37 -0500

    minor unit test fixes

--- backend/tests/main_test.py
+++ backend/tests/main_test.py
@@ -1,25 +1,29 @@
 from fastapi.testclient import TestClient
 
+
 def test_health(test_client: TestClient):
     response = test_client.get("/health")
     assert response.status_code == 200
-    assert response.text == "\"Service is up\""
+    assert response.text == '"Service is up"'
+
 
 def test_topics(test_client: TestClient):
     response = test_client.get("/topics")
     assert response.status_code == 200
 
+
 def test_datasets_without_topic(test_client: TestClient):
     response = test_client.get("/datasets")
     assert response.status_code == 200
 
+
 def test_datasets_with_topic(test_client: TestClient):
-    topic = "Topic1" # may need to be changed
+    topic = "academic"  # may need to be changed
     response = test_client.get(f"/datasets?topic={topic}")
     assert response.status_code == 200
 
+
 def test_dataset(test_client: TestClient):
-    uid = "1" # may need to be changed
+    uid = "245"  # may need to be changed
     response = test_client.get(f"/datasets/{uid}")
     assert response.status_code == 200
-
