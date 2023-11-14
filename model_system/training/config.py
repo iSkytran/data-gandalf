7279b5304407e50ac547250769a711ff878dd56d
Joshua Mason <jlmason6@ncsu.edu>
2023-11-14 12:27:16 -0500

    model version history

--- model_system/training/config.py
+++ model_system/training/config.py
@@ -1,27 +1,38 @@
+from datetime import datetime
+
+# Get time
+current_datetime = datetime.now()
+year = current_datetime.year
+month = current_datetime.month
+day = current_datetime.day
+hour = current_datetime.hour
+minute = current_datetime.minute
+second = current_datetime.second
+
 # Where to save trained model
-MODEL_PATH = "../backend/models/test.pkl"
+MODEL_PATH = f"../backend/models/{year}-{month:02d}-{day:02d}_{hour:02d}:{minute:02d}:{second:02d}_model.pkl"
 
 # DB Configurations
-DBNAME="training_database"
-USER="postgres"
-PASSWORD="default"
-HOST="localhost"
-PORT="5432"
-TABLENAME="dataset"
+DBNAME = "training_database"
+USER = "postgres"
+PASSWORD = "default"
+HOST = "localhost"
+PORT = "5432"
+TABLENAME = "dataset"
 
 # Text columns to be used as input into TF-IDF
 COLS_TO_CLEAN = ["topic", "title", "description"]
 # These default column names match the pg_dump.sql file
-ID_COL ="id"
-LICENSES_COL="licenses"
-TAGS_COL="tags"
-COLUMN_NAMES_COL="col_names"
+ID_COL = "id"
+LICENSES_COL = "licenses"
+TAGS_COL = "tags"
+COLUMN_NAMES_COL = "col_names"
 
 # Tokenization method used by the TF-IDF Model
-TOKENIZATION_METHOD="scibert"
+TOKENIZATION_METHOD = "scibert"
 
 # Weights of specific features on final recommendation scores
 # To deactivate a feature, set feature weight to 1
-LICENSES_WEIGHT=1.1
-TAGS_WEIGHT=1.1
-COLUMN_NAMES_WEIGHT=1.05
+LICENSES_WEIGHT = 1.1
+TAGS_WEIGHT = 1.1
+COLUMN_NAMES_WEIGHT = 1.05
