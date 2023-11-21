e3575eb8f520dd4602e4d05e1620351904db0787
David White <dawhite4@ncsu.edu>
2023-11-21 15:57:33 -0500

    Fixed strange positioning of tooltip icon

--- frontend/app/components/gridItem.tsx
+++ frontend/app/components/gridItem.tsx
@@ -7,7 +7,6 @@ export default function GridItem({
   metadata: any;
   children?: React.ReactNode;
 }) {
-  console.log(metadata.similarityStyle);
   return (
     <div className="w-full shadow-lg rounded-lg p-6 border overflow-x-hidden overflow-y-scroll bg-white">
       <Link href={`/dataset/${metadata.id}`}>
@@ -22,17 +21,18 @@ export default function GridItem({
           {metadata.topic}
         </p>
         {metadata.similarity && (
-          <p className="flex-initial text-l max-w-fit border-medium_blue bg-blue-100 text-midnight_blue border-2 rounded my-2 px-2.5 py-0.5 font-semibold"
+          <div className="flex flex-row justify-center text-l border-medium_blue bg-blue-100 text-midnight_blue border-2 rounded my-2 pl-2.5 pr-1 py-0.5 font-semibold"
             style={metadata.similarityStyle}>
-              {Math.round(100 * metadata.similarity) / 100}
-          </p>
+              <p>
+                {Math.round(100 * metadata.similarity) / 100}
+              </p>
+              <p style={{borderRadius: '50%', alignSelf: "center"}} className="text-gray-700 bg-gray-300 border-2 flex items-center rounded-full h-5 w-5 justify-center">?</p>
+          </div>
         )}
       </div>
 
 
       {children}
-
-
 
       <p className="my-1">
         <span className="font-semibold">Licenses: </span>
