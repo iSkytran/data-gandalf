b6a11c68068ba75ea87e27df0f985cbc232f0e12
Yousif Mansour <yamansou@ncsu.edu>
2023-11-28 16:55:40 -0500

    Two more unit tests - 80% coverage

--- model_system/tests/model_training_test.py
+++ model_system/tests/model_training_test.py
@@ -1,4 +1,6 @@
 import pytest
+import pickle
+import os
 from pytest_mock import mocker
 import training.model_training as script
 from pathlib import Path
@@ -15,6 +17,16 @@ def mock_metadata():
         'licenses': ['LicenseA', 'LicenseA', 'LicenseB'],
         'tags': [r'{A,B,C}',r'{C,D,"Test WhItESpAcE"}',r'{A,D,"Test Whitespace"}'],
         'col_names': [r'{1,2,3}',r'{4,"5 and 6"}',r'{1,2,3,4}']
+    }).copy()
+
+@pytest.fixture
+def mock_ratings():
+    return pd.DataFrame({
+        'user_session': ['a','a','a','b','b'],
+        'recommend': [True, True, True, False, True],
+        'source_dataset': [1,2,3,1,2],
+        'destination_dataset': [2,1,1,2,1],
+        'id': [1,2,3,4,5]
     }).copy()
 
 def test_get_metadata_from_db_error(mocker):
@@ -35,7 +47,7 @@ def test_tune_rec_matrix_no_weights(mock_metadata,mocker):
     tuned_rec_matrix = script.tune_rec_matrix(rec_matrix,mock_metadata,None)
     assert(tuned_rec_matrix == rec_matrix)
 
-# # test that shared tags appropriately weigh the similarity scores
+# test that shared tags appropriately weigh the similarity scores
 def test_tune_rec_matrix_tags(mock_metadata,mocker):
     weight = 2
     mocker.patch('training.config.LICENSES_WEIGHT', 1)
@@ -57,14 +69,6 @@ def test_tune_rec_matrix_tags(mock_metadata,mocker):
     new_rec_from_1_to_2 = next((rec for rec in tuned_rec_matrix[1] if rec[1] == 2), None)[0]
     assert(new_rec_from_1_to_2 == weight * old_rec_from_1_to_2)
 
-# def test_tune_rec_matrix_main(mock_metadata,mocker):
-#     mocker.patch('training.config.MODEL_PATH', "//////")
-#     mocker.patch("training.model_training.get_metadata_from_db", return_value = mock_metadata)
-#     mocker.patch('training.config.LICENSES_WEIGHT', 1)
-#     mocker.patch('training.config.TAGS_WEIGHT', weight)
-#     mocker.patch('training.config.COLUMN_NAMES_WEIGHT', 1)
-#     assert(True)
-
 def test_tune_rec_matrix_path_error(mocker):
     mocker.patch('training.config.MODEL_PATH', "//////")
     with pytest.raises(SystemExit) as excinfo:
@@ -80,13 +84,39 @@ def test_rating_weight():
     assert(script.rating_weight(0,0) == 1)
     assert(script.rating_weight(50,50) == 1)
 
-    print(f"\n(1,0) -> {script.rating_weight(1,0)}")
-    print(f"\n(2,0) -> {script.rating_weight(2,0)}")
-    print(f"\n(3,0) -> {script.rating_weight(3,0)}")
-    print(f"\n(0,1) -> {script.rating_weight(0,1)}")
-    print(f"\n(0,2) -> {script.rating_weight(0,2)}")
-    print(f"\n(0,3) -> {script.rating_weight(0,3)}")
-    print(f"\n(100,10) -> {script.rating_weight(100,10)}")
-    print(f"\n(10,1) -> {script.rating_weight(10,1)}")
-    print(f"\n(10,100) -> {script.rating_weight(10,100)}")
-    print(f"\n(1,10) -> {script.rating_weight(1,10)}")
+def assert_key_error(callable):
+    try:
+        callable()
+        raise AssertionError("Expected KeyError but no exception was raised.")
+    except KeyError:
+        pass  # Expected exception
+    except Exception as e:
+        raise AssertionError("Unexpected exception: {}".format(e))
+
+def test_realign_ratings(mock_ratings):
+    ratings = script.realign_ratings(mock_ratings)
+    assert(ratings.loc[(1,2)][0] == (1,1))
+    assert_key_error(lambda: ratings.loc[(1,3)])
+    assert(ratings.loc[(2,1)][0] == (2,0))
+    assert_key_error(lambda: ratings.loc[(2,3)])
+    assert(ratings.loc[(3,1)][0] == (1,0))
+    assert(ratings.loc[(3,2)][0] == (0,0))
+
+def test_finalized_model(mocker, mock_metadata, mock_ratings):
+    model_path = 'test.pkl'
+    mocker.patch('training.config.MODEL_PATH', model_path)
+    mocker.patch('training.model_training.get_metadata_and_ratings_from_db', 
+                 return_value=(mock_metadata,mock_ratings))
+
+    # Create a new model and serialize it
+    script.main()
+    
+    # Deserialize the model then delete it
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    os.remove(model_path)
+
+    assert(len(model) == 3)
+    assert(len(model[1]) == 2)
+    assert(len(model[2]) == 2)
+    assert(len(model[3]) == 2)
